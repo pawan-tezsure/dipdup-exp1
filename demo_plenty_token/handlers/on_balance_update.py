3bbb969c9b7c6d9e1c3056d1803ce59d20b9dced
Ubuntu <ubuntu@ip-172-31-37-195.ap-south-1.compute.internal>
2021-06-25 09:10:12 +0000

    Somewhat working

--- demo_plenty_token/handlers/on_balance_update.py
+++ demo_plenty_token/handlers/on_balance_update.py
@@ -5,10 +5,10 @@ import demo_plenty_token.models as models
 from demo_plenty_token.types.plenty.big_map.balances_key import BalancesKey
 from demo_plenty_token.types.plenty.big_map.balances_value import BalancesValue
 
-
+from decimal import Decimal
 async def on_balance_update(
     ctx: HandlerContext,
     balances: BigMapDiff[BalancesKey, BalancesValue],
 ) -> None:
-    print(BalancesKey)
-    newHolder, _ = await models.Holder.get_or_create(holderAddress = balances.key.__root__,defaults=dict(balance=balances.value.bakance))
+    finalBalance = Decimal (balances.value.balance) / (10 ** 18)
+    newHolder, _ = await models.Holder.get_or_create(holderAddress = balances.key.__root__,defaults=dict(balance=finalBalance))
