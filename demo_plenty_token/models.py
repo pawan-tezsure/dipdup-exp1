98f9dd71ccc7212728bbbf67cd7a9dccfeff2b96
Pawan Dhanwani <pawan@tezsure.com>
2021-06-25 12:49:02 +0530

    adding some limitation

--- demo_plenty_token/models.py
+++ demo_plenty_token/models.py
@@ -2,5 +2,5 @@ from tortoise import Model, fields
 
 
 class Holder(Model):
-    holderAddress = fields.CharField()
-    balance = fields.BigIntField()
+    holderAddress = fields.CharField(max_length=100)
+    balance = fields.BigIntField(null=False)
